ef8b8ffb3cfd934c9d8cc0beda6d0b795b389770
macos <macos@macoss-MacBook-Air.local>
2021-03-11 19:24:34 +0400

    simple edits

--- main.py
+++ main.py
@@ -4,3 +4,5 @@ if __name__ == "__main__":
     connection = Connection(api_key='c9426ee5181dca77e9a2', user_id='1055')
     data, _ = connection.create_a_team(name='hellos')
     print(data['code'])
+
+    #isitend - teamId - 1248
