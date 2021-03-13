12904e7eba0e726aecc2143eb30980d731cc0241
Rarblack <therarblack@gmail.com>
2021-03-13 13:46:49 +0400

    fix code errors

--- helpers.py
+++ helpers.py
@@ -33,7 +33,7 @@ def is_terminal(player):
 
 				# STARTING FROM THE NEXT POSITION
 				for k in range(j+1, target):
-					if (board[j][i] != board[j+k][i]): break;
+					if (board[j][i] != board[j+k][i]): break
 					if (k == target - 1): win = True
 				if (win):
 					print("column found")
@@ -46,9 +46,9 @@ def is_terminal(player):
 			if (board[i][j] != '-' and j+target <= n):
 				for k in range(target):
 					if (i+target <= n):
-						if (board[i][j] != board[i+k][j+k]): break;
+						if (board[i][j] != board[i+k][j+k]): break
 					else:
-						if (board[i][j] != board[i-k][j+k]): break;
+						if (board[i][j] != board[i-k][j+k]): break
 					if (k == target - 1): win = True
 				if (win):
 					print("diagonal found")
